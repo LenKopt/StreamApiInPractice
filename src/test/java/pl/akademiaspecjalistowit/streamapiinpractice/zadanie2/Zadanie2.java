e7f142bad8d3e206485c06e46e95e543108077fe
Alena Iadkouskaya <koptewka@mail.ru>
2024-03-14 09:44:31 +0100

    zadanie 2 + test

--- src/test/java/pl/akademiaspecjalistowit/streamapiinpractice/zadanie2/Zadanie2.java
+++ src/test/java/pl/akademiaspecjalistowit/streamapiinpractice/zadanie2/Zadanie2.java
@@ -4,24 +4,38 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Zadanie2 {
 
     /**
-     *
      * ZACZNIJ OD NAPISANIA ASERCJI!
-     *
+     * <p>
      * twórz funkcję, która przyjmuje listę słów i zwraca mapę,
      * gdzie kluczem jest długość słowa, a wartością ilość słów o tej długości.
      */
     @Test
-    void grouping(){
+    void grouping() {
+        //given
         List<String> words = Arrays.asList("apple", "banana", "cherry", "date", "fig", "grape");
+        //when
+        Map<Integer, Long> wordCount = groupWords(words);
+        //then
+        assertThat(wordCount.size()).isEqualTo(4);
+        assertThat(wordCount.get(3)).isEqualTo(1);
+        assertThat(wordCount.get(4)).isEqualTo(1);
+        assertThat(wordCount.get(5)).isEqualTo(2);
+        assertThat(wordCount.get(6)).isEqualTo(2);
+    }
 
-        Map<Integer, Long> wordCount = null;
-
-        System.out.println("Mapa długości słów do ich ilości: " + wordCount);
-
+    private Map<Integer, Long> groupWords(List<String> words) {
+        return words.
+                stream().
+                map(k -> k.length()).
+                peek(System.out::println).
+                collect(Collectors.groupingBy(k -> k, Collectors.counting()));
     }
 }
