ff5779a2ff0f2d1ae208ce64f502ef5f6c3dd347
Alena Iadkouskaya <koptewka@mail.ru>
2024-03-16 09:57:44 +0100

    clean code

--- src/test/java/pl/akademiaspecjalistowit/streamapiinpractice/zadanie5/Zadanie5.java
+++ src/test/java/pl/akademiaspecjalistowit/streamapiinpractice/zadanie5/Zadanie5.java
@@ -3,14 +3,16 @@ package pl.akademiaspecjalistowit.streamapiinpractice.zadanie5;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Zadanie5 {
 
     /**
      * ZACZNIJ OD NAPISANIA ASERCJI!
-     *
+     * <p>
      * Masz listę produktów, gdzie każdy produkt zawiera nazwę, cenę oraz kategorię
      * (np. "Elektronika", "Dom", "Ogród").
      * <p>
@@ -23,20 +25,20 @@ public class Zadanie5 {
     void zadanie5() {
         //given
         List<Product> products = List.of(
-            new Product("Telefon", 250.0, "Elektronika"),
-            new Product("Laptop", 1200.0, "Elektronika"),
-            new Product("Kosiarka", 150.0, "Ogród"),
-            new Product("Biurko", 85.0, "Dom"),
-            new Product("Ładowarka", 25.0, "Elektronika"),
-            new Product("Grill", 300.0, "Ogród"),
-            new Product("Lampa", 50.0, "Dom"),
-            new Product("Zestaw narzędzi", 200.0, "Ogród"),
-            new Product("Fotel", 400.0, "Dom"),
-            new Product("Kamera", 150.0, "Elektronika")
+                new Product("Telefon", 250.0, "Elektronika"),
+                new Product("Laptop", 1200.0, "Elektronika"),
+                new Product("Kosiarka", 150.0, "Ogród"),
+                new Product("Biurko", 85.0, "Dom"),
+                new Product("Ładowarka", 25.0, "Elektronika"),
+                new Product("Grill", 300.0, "Ogród"),
+                new Product("Lampa", 50.0, "Dom"),
+                new Product("Zestaw narzędzi", 200.0, "Ogród"),
+                new Product("Fotel", 400.0, "Dom"),
+                new Product("Kamera", 150.0, "Elektronika")
         );
 
         //when
-        Map<String, List<String>> productsGroupedByCategory = findProductsMoreExpensiveThan(products, 100);
+        Map<String, List<String>> productsGroupedByCategory = findProductsMoreExpensiveThan(products);
         //then
         assertThat(productsGroupedByCategory.size()).isEqualTo(3);
         assertThat(productsGroupedByCategory.get("Elektronika").size()).isEqualTo(3);
@@ -44,11 +46,12 @@ public class Zadanie5 {
         assertThat(productsGroupedByCategory.get("Dom").size()).isEqualTo(1);
         assertThat(productsGroupedByCategory.get("Dom").get(0)).isEqualTo(productsGroupedByCategory.get("Dom").get(0).toUpperCase());
     }
-    private Map<String, List<String>> findProductsMoreExpensiveThan(List<Product> products, int price) {
-        return products.
-                stream().
-                filter(p -> p.getPrice() > price).
-                collect(Collectors.groupingBy(Product::getCategory, Collectors.mapping(d -> d.getName().toUpperCase(), Collectors.toList())));
+
+    private Map<String, List<String>> findProductsMoreExpensiveThan(List<Product> products) {
+        return products
+                .stream()
+                .filter(p -> p.getPrice() > 100)
+                .collect(Collectors.groupingBy(Product::getCategory, Collectors.mapping(d -> d.getName().toUpperCase(), Collectors.toList())));
     }
 
 }
